df9240432ccc25e7d890cb184801fcfbb3584979
unknown <eivind.rydningen@gmail.com>
2021-03-09 18:00:30 +0100

    bla

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:rockets/models/rocket.dart';
+import 'package:rockets/screens/home/rockets/rocket_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Todo: Add  ann appBar to the Scaffold Widget with the title 'Rocket Guide'
     return Scaffold(
-
+        appBar: AppBar(
+            title: const Text('Rocket Guide')
+        ),
         // Todo: Replace the Center and Text Widget with the RocketList
-        body: Center(
-      child: Text(
-        'Hello Online! 🚀',
-        style: TextStyle(fontSize: 30),
-      ),
-    ));
+        body: RocketList()
+    );
   }
 }
